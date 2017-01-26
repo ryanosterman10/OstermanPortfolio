f6fa37c199f062c7098fe7f04c9442b6d512c865
Ryan Osterman <ryan_osterman@med.unc.edu>
2017-01-26 13:40:31 -0500

    initial commit set up server and package.json

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+//Include the path package to get the correct file path
+var path = require('path');
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(__dirname + '/public'));
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+//root route redirect to /index
+
+app.get("/", function(req, res) {
+  res.render(index.html);
+});
+
+// Set up the server
+var PORT = process.env.PORT || 8000; // Sets an initial port.
+app.listen(PORT, function() {
+  console.log('App Server is listening on port ' + PORT);
+});
